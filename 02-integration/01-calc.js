9f0314f47b8e0694a3dc8fbfae1cda4e0f513ea7
andres <andresponton21@yahoo.com>
2021-03-30 17:57:52 -0400

    Finished with integrational tests

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -3,9 +3,15 @@
 // ==================================================
 function calc(operation, num1, num2) {
         switch (operation){
+
           case 'add': return num1 + num2;
-        
           case 'subtract': return num1-num2;
+          case 'multiply': return num1*num2;
+          case 'divide': return num1/num2;
+          default: return console.log(`operation not supported`);
+       
+          
+
         }
 }
 
@@ -25,22 +31,26 @@ try {
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
   var result = calc('subtract', 20, 10);
-  if (result !== 10) throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
-
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
+ var result = calc('exponent', 2, 8);
+  
 
   console.log('All tests passed successfully.');
 
