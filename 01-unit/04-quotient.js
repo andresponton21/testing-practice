e963cc29efc4cd7a40c8226efb1daf59db325fc8
andres <andresponton21@yahoo.com>
2021-03-29 23:19:29 -0400

    finished w divide

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,7 +1,11 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
+function quotient(num1, num2) {
+  if(num1===undefined){num1=0}
+
+  if(num2===undefined){num2=1}
+    return num1 / num2
 
 }
 
@@ -20,22 +24,27 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(2, 1);
+  if (result === Infinity) throw new Error('Error you can not divide a number by 0' );
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = quotient(2, 2, 3);
+  if (result !== 1) throw new Error('Expected quotient(2, 2, 3) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = quotient( );
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
